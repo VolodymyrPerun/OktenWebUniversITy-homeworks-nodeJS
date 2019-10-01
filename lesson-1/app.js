f6f898697b5e3bc5631b6123e666fa80440ecb92
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2019-10-01 21:42:40 +0300

    Initial commit

---
+++ lesson-1/app.js
@@ -0,0 +1,27 @@
+const myFile = require('./myFile');
+const fs = require('fs');
+
+fs.mkdir('./myDir',() => {
+    console.log('created');
+});
+
+for (let i = 0; i < 10; i++) {
+
+    fs.appendFile('./myDir/text.txt', '\n Hello text!',() => {
+        console.log('created text');
+    });
+}
+
+fs.readFile('./text.txt', ((err, data) => {
+console.log(err, data.toString())
+}));
+
+let readStream = fs.createReadStream('./text.txt');
+readStream.on('data', data =>{
+    console.log(data.toString());
+});
+
+let writeStream = fs.createWriteStream('./SomeFile.txt');
+writeStream.write('Some Text');
+
+console.log(myFile);
---
+++ lesson-1/myFile.js
@@ -0,0 +1,3 @@
+let xxx = 'Hello';
+
+module.exports = xxx;
