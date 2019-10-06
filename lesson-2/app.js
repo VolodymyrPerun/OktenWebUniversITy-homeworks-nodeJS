c2938f10663958fcbd9215b3f686f19a30a383c3
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2019-10-06 16:12:30 +0300

    change lesson-2

--- lesson-2/app.js
+++ lesson-2/app.js
@@ -4,18 +4,51 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.get('/',(req, res) => {
-    console.error(__filename);
-    console.error(__dirname);
-    console.error(path.join(__dirname, 'static'));
-res.end('ok')
+const users = [];
+
+app.engine('.hbs', expHbs({
+    extname: '.hbs'
+}));
+
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'static'));
+
+app.get('/login', (req, res) => {
+    res.render('main', { title: 'HELLO WORLD', group: 'jscx-july-19'})
 });
 
-app.get('/login',(req, res) => {
-    res.end('login page')
+app.post('/register', (req, res) => {
+    let body = req.body;
+    users.push(body);
+    res.render('login')
 });
+
+app.get('/users', (req,res)=> {
+    res.end(JSON.stringify(users));
+    res.render('register')
+});
+
+app.get('/users/:user_id', (req,res)=> {
+    console.log(req.params);
+    console.log(req.query);
+    res.end(JSON.stringify(req.params))
+});
+
+// app.all('*', (req, res)=> {
+//     res.status(505).json('NOT FOUND? SORRY')
+// });
+
+
+
+// app.use('*', (req, res) => {
+//     console.log('error 404');
+//     res.sendFile(path.join(__dirname, 'static', '404.html'));
+// });
 
 app.listen(3000, () => {
     console.log('Server listen on port 3000...');
