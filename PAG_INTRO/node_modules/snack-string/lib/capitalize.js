5052050a9ac80cb0d342370ef2ec774095aecaf5
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2019-11-16 20:12:46 +0200

    delete idea and node modules file

--- PAG_INTRO/node_modules/snack-string/lib/capitalize.js
+++
@@ -1,6 +0,0 @@
-var toString = require('./toString');
-
-module.exports = function capitalize(string) {
-    string = toString(string);
-    return string.charAt(0).toUpperCase() + string.substring(1);
-};
--- homework-lun/node_modules/snack-string/lib/decapitalize.js
+++
@@ -1,6 +0,0 @@
-var toString = require('./toString');
-
-module.exports = function decapitalize(string) {
-    string = toString(string);
-    return string.charAt(0).toLowerCase() + string.substring(1);
-};
